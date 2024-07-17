7b555af35f1218208d2388e20c5ef8df92f58ba8
ArbeniOSDev <162730156+ArbeniOSDev@users.noreply.github.com>
2024-07-17 13:33:32 +0200

    Add files via upload

---
+++ net360/net360/net360App.swift
@@ -0,0 +1,17 @@
+//
+//  net360App.swift
+//  net360
+//
+//  Created by Arben on 17.7.24.
+//
+
+import SwiftUI
+
+@main
+struct net360App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
