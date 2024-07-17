259333eeba05d1b236df8c04569984549cc17669
ArbeniOSDev <162730156+ArbeniOSDev@users.noreply.github.com>
2024-07-17 13:33:02 +0200

    Initial Commit

---
+++ net360/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  net360
+//
+//  Created by Arben on 17.7.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
