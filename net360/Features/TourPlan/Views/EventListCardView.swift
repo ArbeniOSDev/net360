20feabb18b791f0b239dde3dde09e32c6eda482e
Besim Shaqiri <besimshaqiri@gmail.com>
2024-07-23 17:15:19 +0200

    update Ui in EventListCardView

--- net360/Features/TourPlan/Views/EventListCardView.swift
+++ net360/Features/TourPlan/Views/EventListCardView.swift
@@ -18,70 +18,71 @@ struct EventListCardView: View {
     @State private var navigateToDetails = false
     
     var body: some View {
-        HStack(spacing: 20) {
-            VStack(alignment: .leading, spacing: 8) {
-                VStack(alignment: .leading, spacing: 4) {
-                    SubTextBold(event?.location ?? "Basel", 22, .bold, color: Color(hex: "#07314C"))
-                    SubTextBold(event?.venue ?? "Allmend", 22, .bold, color: Color(hex: "#07314C"))
-                }.padding(.bottom, 10)
+        HStack {
+            VStack(spacing: 15) {
                 HStack {
                     VStack(alignment: .leading) {
-                        DescText("Start", 12, color: .black)
-                        SubTextBold("02", 20, color: .black)
-                        DescText("AUG", 12, color: .black)
+                        SubTextBold(event?.location ?? "Basel", 22, .bold, color: Color(hex: "#07314C"))
+                        SubTextBold(event?.venue ?? "Allmend", 22, .bold, color: Color(hex: "#07314C"))
                     }
-                    VStack(alignment: .leading) {
-                        DescText("End", 12, color: .black)
-                        SubTextBold("04", 20, color: .black)
-                        DescText("AUG", 12, color: .black)
-                    }.padding(.trailing, 20)
-                    VStack(alignment: .center) {
-                        DescText("Total", 12, color: .black)
-                        SubTextBold("15", 20, color: .black)
-                        DescText("Events", 12, color: .black)
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        SubTextBold(eventType == .future ? "15 Days to Start" : "Expired", 16, .bold, color: .white)
+                            .padding(.vertical, 4)
+                            .padding(.horizontal, 8)
+                            .background(eventType == .future ? Color(hex: "#00A3FF") : Color(hex: "#9D6EFF"))
+                            .cornerRadius(5)
+                        SubTextBold("2 Days", 16, .bold, color: .white)
+                            .padding(.vertical, 4)
+                            .padding(.horizontal, 8)
+                            .background(eventType == .future ? Color(hex: "#00A3FF") : Color(hex: "#9D6EFF"))
+                            .cornerRadius(5)
                     }
                 }
-            }.frame(maxWidth: .infinity)
-            Spacer()
-            VStack(alignment: .trailing, spacing: 20) {
-                VStack(alignment: .trailing) {
-                    SubTextBold(eventType == .future ? "15 Days to Start" : "Expired", 16, .bold, color: .white)
-                        .padding(.vertical, 4)
-                        .padding(.horizontal, 8)
-                        .background(eventType == .future ? Color(hex: "#00A3FF") : Color(hex: "#9D6EFF"))
-                        .cornerRadius(5)
-                    SubTextBold("2 Days", 16, .bold, color: .white)
-                        .padding(.vertical, 4)
-                        .padding(.horizontal, 8)
-                        .background(eventType == .future ? Color(hex: "#00A3FF") : Color(hex: "#9D6EFF"))
-                        .cornerRadius(5)
-                }
-                .padding(.bottom, 10)
-                Button(action: {
-                    
-                }) {
-                    HStack {
-                        NavigationLink(destination: DetailsEventListView(), isActive: $navigateToDetails) {
-                            Spacer()
-                            Button {
-                                navigateToDetails = true
-                            } label: {
-                                SubTextBold("Zeitpläne", 16, .bold, color: .black)
-                            }
-                            Spacer()
+                Spacer()
+                HStack {
+                    HStack(spacing: 10) {
+                        VStack(alignment: .center) {
+                            DescText("Start", 12, color: .black)
+                            SubTextBold("02", 20, color: .black)
+                            DescText("AUG", 12, color: .black)
                         }
+                        VStack(alignment: .center) {
+                            DescText("End", 12, color: .black)
+                            SubTextBold("04", 20, color: .black)
+                            DescText("AUG", 12, color: .black)
+                        }
+                        VStack(alignment: .center) {
+                            DescText("Total", 12, color: .black)
+                            SubTextBold("15", 20, color: .black)
+                            DescText("Events", 12, color: .black)
+                        }.padding(.leading, 20)
                     }
-                    .padding()
-                    .background(eventType == .future ? Color(hex: "#0BFFD3") : Color(hex: "#C3FE45"))
+                    Spacer()
+                    Button(action: {
+                        
+                    }) {
+                        HStack {
+                            NavigationLink(destination: DetailsEventListView(), isActive: $navigateToDetails) {
+                                Spacer()
+                                Button {
+                                    navigateToDetails = true
+                                } label: {
+                                    SubTextBold("Zeitpläne", 16, .bold, color: .black)
+                                }
+                                Spacer()
+                            }
+                        }.verticalPadding()
+                            .background(eventType == .future ? Color(hex: "#0BFFD3") : Color(hex: "#C3FE45"))
+                    }
+                    .frame(width: 140)
+                    .cornerRadius(10)
                 }
-                .frame(width: 140)
-                .cornerRadius(10)
             }
-        }
-        .padding(20)
-        .background(Color.white)
-        .cornerRadius(15)
-        .shadow(color: .gray.opacity(0.9), radius: 6, x: 0, y: 4)
+        }.padding(20)
+            .background(Color.white)
+            .cornerRadius(15)
+            .shadow(color: .gray.opacity(0.9), radius: 6, x: 0, y: 4)
     }
 }
 
